700b8c8343aa2b5402a63051a98df6b38c37a0e4
sjardon <santiagohernanjardon@gmail.com>
2021-09-29 16:29:56 -0300

    #dkzgm3 add mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AllExceptionFilter } from './core/all-exeptions.filter';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL || ''),
+  ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_FILTER,
-    useClass: AllExceptionFilter
-  }],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
